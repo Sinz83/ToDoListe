4b0e78c02a061b000dc1b3c103513cf48a792909
sabrina <petitcomiteparis@gmail.com>
2022-09-16 13:30:18 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sabrina Inzillo on 16.09.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
